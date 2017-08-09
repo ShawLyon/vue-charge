99101beb877eceb6d34250a7cccd00bc47a0f754
lyon <lyon19930914@gmail.com>
2017-08-09 18:47:33 +0800

    高德官方

--- src/components/ChargeMap/ChargeMap.vue
+++ src/components/ChargeMap/ChargeMap.vue
@@ -1,61 +1,85 @@
 <template>
-  <div id="map-container">
-    <el-amap vid="amapDemo" :zoom="zoom" :center="center" :plugin="plugins">
-      <el-amap-marker v-for="marker in markers" :position="marker.position"></el-amap-marker>
-      <el-amap-ground-image v-for="groundimage in groundimages" :url="groundimage.url" :bounds="groundimage.bounds" :events="groundimage.events"></el-amap-ground-image>
-    </el-amap>
+  <div id="container">
   </div>
 </template>
+
 <script>
+
 export default {
+  mounted() {
+    // 百度
+    /*  // 1. 创建地图实例
+     var map = new BMap.Map("map-container");
+     // 2. 创建点坐标
+     var point = new BMap.Point(116.404, 39.915);
+     // 3. 地图初始化
+     map.centerAndZoom(point, 15); */
+
+    // 高德
+    // 1.初始化
+    var map = new AMap.Map('container', {
+      resizeEnable: true,
+      zoom: 12,
+      center: [116.480983, 40.0958]
+    });
+    // 2.遮盖物
+    var marker = new AMap.Marker({
+      position: [113.907419, 22.498795], //marker所在的位置
+      offset: new AMap.Pixel(-17, -42), //相对于基点的偏移位置
+      icon: new AMap.Icon({
+        size: new AMap.Size(40, 50),  //图标大小
+        image: "http://webapi.amap.com/theme/v1.3/images/newpc/way_btn2.png",
+        imageOffset: new AMap.Pixel(0, -60)
+      }),
+      content: '<div class="marker-route marker-marker-bus-from"></div>',  //自定义点标记覆盖物内容
+      map: map//创建时直接赋予map属性
+    });
+    //也可以在创建完成后通过setMap方法执行地图对象
+    marker.setMap(map);
+
+
+    // 3.插件-控件
+    AMap.plugin(['AMap.ToolBar', 'AMap.Scale', 'AMap.Geolocation'], function () {
+
+      var toolBar = new AMap.ToolBar(); //创建并添加工具条控件
+      var scale = new AMap.Scale(); //缩放控件
+      var geolocation = new AMap.Geolocation({
+        buttonPosition: 'RB',
+        zoomToAccuracy: true,
+        buttonOffset: new AMap.Pixel(14, 135)
+      }); //定位控件
+
+      map.addControl(toolBar);
+      map.addControl(scale);
+      map.addControl(geolocation);
+      geolocation.getCurrentPosition(); // 获取当前定位
+      AMap.event.addListener(geolocation, 'complete', onComplete);//返回定位信息
+    })
+  },
   data() {
-    let self = this;
-    return {
-      zoom: 10,
-      center: [113.814549, 22.63336],
-      lng: 0,
-      lat: 0,
-      plugins: ['ToolBar', {
-        pName: 'Geolocation',
-        events: {
-          init(o) {
-            // o 是高德地图定位插件实例
-            o.getCurrentPosition((status, result) => {
-              if (result && result.position) {
-                self.lng = result.position.lng;
-                self.lat = result.position.lat;
-                self.center = [self.lng, self.lat];
-                self.$nextTick();
-              }
-            });
-          }
-        }
-      }],
-      markers: [{
-        position: [113.879037, 22.548737]
-      }, {
-        position: [113.942788, 22.547231]
-      }],
-      groundimages: [
-        {
-          url: '/jiaoliu.png',
-          bounds: [[121.5273285, 31.21515044], [122.9273285, 32.31515044]],
-          events: {
-            click() {
-              alert('click groundimage');
-            }
-          }
-        }
-      ]
-    }
+
   }
 }
 </script>
-<style lang="less" scoped>
+<style lang="less" >
 #map-container {
   // margin-top: 46px;
-  height: 100%;
-  width: 100%;
+  // height: 100%;
+  // width: 100%;
+  // overflow:hidden;
+}
+
+.amap-marker .marker-route {
+  position: absolute;
+  width: 40px;
+  height: 44px;
+  color: #e90000;
+  background: url('zhiliu.png') no-repeat;
+  cursor: pointer;
+}
+
+.amap-marker .marker-marker-bus-from {
+  background-position: -334px -180px;
 }
 </style>
 
